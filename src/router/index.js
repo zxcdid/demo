60fe41a9fa00db4bf8a97a5d6ee7ebe7febaef22
Administrator <2069252750@qq.com>
2022-09-09 17:20:14 +0800

    完成了demo的搭建和删除方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Pie from '../views/Pie.vue'
+import Table from '../views/Table.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Pie
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/bar',
+    name: 'bar',
+    component: () => import('../views/Bar.vue')
+  },
+  {
+    path: '/tabel',
+    name: 'tabel',
+    component: Table
   }
 ]
 
---
+++ src/views/Pie.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="myChart" :style="{width: '800px', height: '600px'}"></div>
+</template>
+<script>
+export default {
+  name: 'Echarts',
+  mounted(){
+    // 基于准备好的dom，初始化echarts实例
+    let myChart = this.$echarts.init(document.getElementById('myChart'))
+    // 绘制图表
+    myChart.setOption({
+      backgroundColor: '#2c343c',
+      title: {
+        text: 'Customized Pie',
+        left: 'center',
+        top: 20,
+        textStyle: {
+          color: '#ccc'
+        }
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1]
+        }
+      },
+      series: [
+        {
+          name: 'Access From',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: [
+            { value: 335, name: 'Direct' },
+            { value: 310, name: 'Email' },
+            { value: 274, name: 'Union Ads' },
+            { value: 235, name: 'Video Ads' },
+            { value: 400, name: 'Search Engine' }
+          ].sort(function (a, b) {
+            return a.value - b.value;
+          }),
+          roseType: 'radius',
+          label: {
+            color: 'rgba(255, 255, 255, 0.3)'
+          },
+          labelLine: {
+            lineStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20
+          },
+          itemStyle: {
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          },
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: function (idx) {
+            return Math.random() * 200;
+          }
+        }
+      ]
+    });
+  }
+}
+</script>
